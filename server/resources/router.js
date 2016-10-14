2478c42698c0dd2a8a9a0132f5ab521af8e6f1bf
Bradford Melluish <bradmelluish@gmail.com>
2016-10-14 13:47:10 -0700

    Implement Ticket Exporting
    Exports ticket tables.

--- server/resources/router.js
+++ server/resources/router.js
@@ -1,8 +1,6 @@
 const router = require('../config/middleware.js').router();
 const api = require('./controller.js');
 
-// router.get('/api/:id/stub', api.getStubs); //return stubs. this will be how the kb search service updates
-
 router.route('/metrics')
   .get(api.getRelations);
 
@@ -14,10 +12,6 @@ router.route('/api/:id')
   .get(api.getTicket)
   .put(api.editTicket)
   .delete(api.deleteTicket);
-
-// router.route('/api/:ticketId/:articleId')
-//   .post(api.associateArticle)
-
 
 router.get('/', api.pingDb)
 
