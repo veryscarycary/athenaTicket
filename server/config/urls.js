9141bca59621de9fb709af958430d00b63b18108
Cary Meskell <carymeskell@gmail.com>
2024-10-18 21:46:30 -0700

    db auth from ENV

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,10 +1,12 @@
 'use strict'
+const DB_AUTH = require('./dbAuth.js');
+
 const DIALECT = 'postgres';
 const HOST = 'localhost';
 const PORT = 5432;
 const DB = 'ticket';
-const DB_USR = require('./dbAuth.js').DB_USR;
-const DB_PASS = require('./dbAuth.js').DB_PASS;
+const DB_USR = process.env.DB_USR || DB_AUTH.DB_USR;
+const DB_PASS = process.env.DB_PASS || DB_AUTH.DB_PASS;
 
 module.exports = {
   default: 3002,
